04b57285ce2b7bc6a976652df891ba6faa8025f6
Олександр Павлов <cheshirelk@gmail.com>
2018-07-27 16:12:58 +0300

    added validation for realEstateCrearion

--- WebUI/Models/RealEstateToSaveView.cs
+++ WebUI/Models/RealEstateToSaveView.cs
@@ -8,18 +8,37 @@ namespace WebUI.Models
 {
     public class RealEstateToSaveView
     {
-        public int Width { set; get; }
+        [Required]
+        [StringLength(7, MinimumLength = 1, ErrorMessage = "Building must be between 1 and 7 characters")]
+        [RegularExpression(@"^[1-9]+\d*\w?(\/?[1-9]+\d*\w?)?", ErrorMessage = "Invalid Building value!")]
         public string Building { get; set; }
-        [RegularExpression(@"^\d*")]
+
+        [Required]
+        [StringLength(4, MinimumLength = 1, ErrorMessage= "Appartment must be between 1 and 4 characters")]
+        [RegularExpression(@"^[1-9]+\d*",ErrorMessage= "Invalid Appartment value!")]
         public string Appartment { get; set; }
-        public Int16 Floor { get; set; }
-        public Int16 Height { get; set; }
-        public float Area { get; set; }
-        public Decimal Price { get; set; }
-        public byte RoomNumber { get; set; }
+
+        [Required]
+        [StringLength(4,  ErrorMessage = "Invalid Floor value!")]
+        [RegularExpression(@"^[1-9]+\d*", ErrorMessage = "Invalid Floor value!")]
+        public string Floor { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[1-9]+\d*", ErrorMessage = "Invalid Height value!")]
+        public string Height { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[1-9]+\d*", ErrorMessage = "Invalid Area value!")]
+        public string Area { get; set; }
+        [Required]
+        [RegularExpression(@"^\d*\.?\d*", ErrorMessage = "Invalid Price value!")]
+        public string Price { get; set; }
+        [Required]
+        [RegularExpression(@"^[1-9]+\d*", ErrorMessage = "Invalid Room Number value!")]
+        public string RoomNumber { get; set; }
         public string Description { get; set; }
         public byte[] Image { get; set; }
-        [RegularExpression(@"^\d*")]
+        [RegularExpression(@"^[1-9]+\d*", ErrorMessage = "Streed need to be choosen!")]
         public int StreetId { get; set; }
         public int DistrictId { get; set; }
     }
